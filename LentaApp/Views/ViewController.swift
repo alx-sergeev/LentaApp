dac96db7c7cc0047f8958ace4054ced9af28ae55
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-02-22 23:43:48 +0400

    Вывод 10 рандомных фотографий, загруженных по API Unsplash

--- LentaApp/Views/ViewController.swift
+++ LentaApp/Views/ViewController.swift
@@ -13,11 +13,21 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     private let cellName = "photoCell"
+    private let networkManager: NetworkManagerProtocol = NetworkManager.shared
+    private var photos: [Photo]! = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         myTableView.dataSource = self
+        
+        let _ = networkManager.fetchData(page: 1, perPage: 10) { [weak self] decodeData in
+            self?.photos = decodeData
+            
+            DispatchQueue.main.async {
+                self?.myTableView.reloadData()
+            }
+        }
     }
 }
 
@@ -27,16 +37,16 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return photos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath) as! PhotoCell
-        
-        cell.titlePhoto.text = "Example text"
+        let row = indexPath.row
         
         DispatchQueue.global().async {
-            guard let URL = URL(string: "https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=400&fit=max") else { return }
+            guard let smallImagePath = self.photos[row].urls?["small"] else { return }
+            guard let URL = URL(string: smallImagePath) else { return }
             guard let imageData = try? Data(contentsOf: URL) else { return }
         
             DispatchQueue.main.async {
@@ -44,10 +54,8 @@ extension ViewController: UITableViewDataSource {
             }
         }
         
-        cell.titlePhoto.text = "My description for photo"
+        cell.titlePhoto.text = photos[row].description ?? ""
         
         return cell
     }
-    
-    
 }
