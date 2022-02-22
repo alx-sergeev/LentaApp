ab61fd5a6a4473aeb5d9ffc8f5547623e452be69
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-02-22 12:59:54 +0400

    Initial Commit

---
+++ LentaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LentaApp
+//
+//  Created by Сергеев Александр on 22.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
