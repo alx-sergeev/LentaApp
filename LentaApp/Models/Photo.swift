289d695dcd89b113cfc2373148fa50c5be367622
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-02-22 23:42:31 +0400

    Добавил модель Photo

---
+++ LentaApp/Models/Photo.swift
@@ -0,0 +1,11 @@
+//
+//  Photo.swift
+//  LentaApp
+//
+//  Created by Сергеев Александр on 22.02.2022.
+//
+
+struct Photo: Codable {
+    let description: String?
+    let urls: [String: String]?
+}
