242230d58cb6b7045019fd99868e3f4d69d9aab0
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-02-23 01:07:14 +0400

    Верстка экрана с детальной информацией

---
+++ LentaApp/Views/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  LentaApp
+//
+//  Created by Сергеев Александр on 23.02.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet weak var detailPhoto: UIImageView!
+    @IBOutlet weak var downloadLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
