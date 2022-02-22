06b1f8fcdc0761639ee92c2ba84637f5e120559d
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-02-22 23:42:54 +0400

    Добавил менеджер NetworkManager

---
+++ LentaApp/Models/Managers/NetworkManager.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkManager.swift
+//  LentaApp
+//
+//  Created by Сергеев Александр on 22.02.2022.
+//
+
+import Foundation
+
+protocol NetworkManagerProtocol {
+    static var shared: NetworkManagerProtocol { get }
+    func fetchData(page: Int, perPage: Int, completion: @escaping ([Photo]) -> Void) -> [Photo]
+}
+
+class NetworkManager: NetworkManagerProtocol {
+    static let shared: NetworkManagerProtocol = NetworkManager()
+    
+    static let apiKey = "2rLiMzqzLQYbNWMN1gmfp9bOYLHbuTBgru3m5dTil7w"
+    static private var apiUrl: String {
+        return "https://api.unsplash.com/photos/?client_id=\(apiKey)"
+    }
+
+    func fetchData(page: Int, perPage: Int, completion: @escaping ([Photo]) -> Void) -> [Photo] {
+        guard let URL = URL(string: "\(NetworkManager.apiUrl)&page=\(page)&per_page=\(perPage)") else { return [] }
+        
+        let photos: [Photo] = []
+        
+        let task = URLSession.shared.dataTask(with: URL) { (data, _, _) in
+            guard
+                let data = data,
+                let decodeData = try? JSONDecoder().decode([Photo].self, from: data)
+                else { return }
+            
+            completion(decodeData)
+        }
+        
+        task.resume()
+        
+        return photos
+    }
+}
